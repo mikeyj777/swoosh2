6bef601e294b14374df7484996156c6fd92ddf7a
Michael James <mikeyj777@gmail.com>
2018-02-26 20:51:34 -0500

    end of lesson

--- swoosh2/swoosh2/ViewController.swift
+++ swoosh2/swoosh2/ViewController.swift
@@ -16,21 +16,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        
-        imgBg.frame = view.frame
- 
-        */
- 
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+    
+        
+    
     }
-
 
 }
 
